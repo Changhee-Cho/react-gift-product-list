6e416ee56f90da5eb56266f878c542998130b4d8
changhee-cho <changhee@kangwon.ac.kr>
2025-07-18 16:17:33 +0900

    fix: 테스트 과정에서 사용했던 console.log() 삭제

--- src/lib/apiOrderClient.ts
+++ src/lib/apiOrderClient.ts
@@ -19,7 +19,6 @@ apiOrderClient.interceptors.response.use(
 
     if (status >= 400 && status < 500) {
       toast.error(message);
-      console.log(error);
     } else if (status >= 500) {
       toast.error('서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.');
     } else {
